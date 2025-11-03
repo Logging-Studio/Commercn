21c196521c6fa8c74e84e3bb75b8b9e4672efa56
ariflogs <devarifhossain@gmail.com>
2025-11-03 23:50:06 +0600

    hello shadcn community 👋

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,19 @@
-import Link from 'next/link';
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
-      <p>
-        You can open{' '}
-        <Link href="/docs" className="font-medium underline">
-          /docs
-        </Link>{' '}
-        and see the documentation.
+      <h1 className="text-4xl mb-4">
+        Shadcn UI Blocks for <br /> E-commerce Websites
+      </h1>
+      <p className="text-muted-foreground mb-6">
+        Ship you e-commerce website fast with pre-built UI Blocks.<br /> Built with React, Tailwind CSS, and Shadcn UI.
       </p>
+
+      <div className="flex gap-4 mx-auto">
+        <Button>Get Started</Button>
+        <Button variant="outline">Browse Blocks</Button>
+      </div>
     </div>
   );
 }

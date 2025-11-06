c718ac9bfd4d9839e0427465b84c7ce11069378b
IAmKrishThakkar <kthakkar894@gmail.com>
2025-11-06 16:16:32 +0530

    Added Promo banners

--- src/components/block-preview.tsx
+++ src/components/block-preview.tsx
@@ -1,4 +1,12 @@
-export function BlockPreview({children}: {children: React.ReactNode}) {
+export function BlockPreview({children, fullWidth = false}: {children: React.ReactNode, fullWidth?: boolean}) {
+    if (fullWidth) {
+        return (
+            <div className="min-h-[200px] not-prose border rounded-xl overflow-hidden">
+                {children}
+            </div>
+        )
+    }
+    
     return (
         <div className="min-h-[500px] not-prose flex flex-col justify-center items-center p-4 lg:p-8 border rounded-xl">
             {children}
